fefb012ca7320dee3a9db55de20c99ed05d5a8ff
Dhaval Patel <dhavalpatel@Dhavals-Laptop.local>
2024-12-11 17:43:03 -0500

    first commit

---
+++ src/main/java/com/example/springboot_docker_demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springboot_docker_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Dockerized Spring Boot!";
+    }
+}
